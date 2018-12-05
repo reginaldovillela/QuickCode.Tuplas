fc98071fef3267ffde64b09bc9ea164cd6d73ce1
Reginaldo Villela <reginaldo.villela@hotmail.com>
2018-12-04 23:43:59 -0200

    First Commit

---
+++ QuickCode.Tuplas/ExemploTuplas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace QuickCode.Tuplas
+{
+    public class ExemploTuplas
+    {
+        public void ExemploUm()
+        {
+            var carro = ("Porsche", "911 GT3 RS");
+
+            Console.WriteLine($"Marca: {carro.Item1}");
+            Console.WriteLine($"Modelo: {carro.Item2}");
+        }
+
+        public void ExemploDois()
+        {
+            var carro = (Marca: "Ferrari", Modelo: "458 Italia");
+
+            Console.WriteLine($"Marca: {carro.Marca}");
+            Console.WriteLine($"Modelo: {carro.Modelo}");
+        }
+
+        public void ExemploTres()
+        {
+            (string, string) carro = ("Audi", "R8 Coupé V10");
+
+            Console.WriteLine($"Marca: {carro.Item1}");
+            Console.WriteLine($"Modelo: {carro.Item2}");
+        }
+
+        public void ExemploQuatro()
+        {
+            (string Marca, string Modelo) carro = ("Mercedes-Benz", "SLS AMG");
+
+            Console.WriteLine($"Marca: {carro.Marca}");
+            Console.WriteLine($"Modelo: {carro.Modelo}");
+        }
+
+        public void ExemploCinco()
+        {
+            (string Marca, string Modelo) = ("Bugatti", "Veyron");
+
+            Console.WriteLine($"Marca: {Marca}");
+            Console.WriteLine($"Modelo: {Modelo}");
+        }
+
+        public (string, string) ExemploSeis()
+        {
+            return ("Pagani", "Huayra");
+        }
+
+        public (string Marca, string Modelo) ExemploSete()
+        {
+            return ("Koenigsegg", "Agera S");
+        }
+    }
+}
---
+++ QuickCode.Tuplas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace QuickCode.Tuplas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tuplas = new ExemploTuplas();
+
+            // Exemplo Um
+            tuplas.ExemploUm();
+
+            // Exemplo Dois
+            tuplas.ExemploDois();
+
+            // Exemplo Três
+            tuplas.ExemploTres();
+
+            // Exemplo Quatro
+            tuplas.ExemploQuatro();
+
+            // Exemplo Cinco
+            tuplas.ExemploCinco();
+
+            // Exemplo Seis
+            var carroSeis = tuplas.ExemploSeis();
+            Console.WriteLine($"Marca: {carroSeis.Item1}");
+            Console.WriteLine($"Modelo: {carroSeis.Item2}");
+
+            // Exemplo Sete
+            var carroSete = tuplas.ExemploSete();
+            Console.WriteLine($"Marca: {carroSete.Marca}");
+            Console.WriteLine($"Modelo: {carroSete.Modelo}");
+
+            var (Marca, Modelo) = tuplas.ExemploSete();
+            Console.WriteLine($"Marca: {Marca}");
+            Console.WriteLine($"Modelo: {Modelo}");
+
+            Console.ReadKey();
+        }
+    }
+}
